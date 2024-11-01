cf69a809d655de85cc366753a53a4c2b2d484587
unknown <okparakelvin65@gmail.com>
2024-11-01 20:59:36 +0100

    first commit in a while

---
+++ Backend/controllers/cartController.js
@@ -0,0 +1,5 @@
+const createCart = async (req, res) => {
+  const user = req.params.user;
+  try {
+  } catch (error) {}
+};
--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,7 +1,37 @@
 import mongoose from "mongoose";
 import userModel from "../models/userModel";
+import httpStatus from "http-status";
 
-export const CreateUser = (req, res) => {};
-export const GetSingleUser = (req, res) => {};
+export const CreateUser = async (req, res) => {
+  const user = req.body;
+
+  try {
+    const usernameExist = await userModel.findOne(
+      { username: user.username } || { email: user.email }
+    );
+    if (usernameExist) {
+      res.status(httpStatus.BAD_REQUEST).json({
+        status: "error",
+        data: "user does not exist",
+      });
+    }
+    return;
+  } catch (error) {}
+};
+export const GetSingleUser = (req, res) => {
+  let userId = req.userId;
+  console.log(userId);
+  if (userId) {
+    res.status(httpStatus.OK).json({
+      status: "success",
+      data: "single user exists",
+    });
+  } else {
+    res.status(httpStatus.BAD_REQUEST).json({
+      status: "Error",
+      data: "single user does not exists",
+    });
+  }
+};
 export const GetAllUsers = (req, res) => {};
 export const RemoveUser = (req, res) => {};
